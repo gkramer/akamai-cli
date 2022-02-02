1471599e0d679ea7e241c82a8f6004e03663541b
Yaniv Cohen <confis@gmail.com>
2022-02-02 13:14:03 +0200

    Update addcrt.sh

--- scripts/addcrt.sh
+++ scripts/addcrt.sh
@@ -4,5 +4,6 @@ while IFS= read -r line
 do
   cat scripts/sample.yml | sed 's/foo.com/'$line'/g' > crt/$line.yml
   # akamai cps create --file /crt/$line.yml --contract-id V-40T9211 --force --section default
+  akamai cps create --file /crt/$line.yml
   echo "domain: $line"
 done < "$File"
