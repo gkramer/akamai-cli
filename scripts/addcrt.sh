669f8c9b1993e249f7f0d2b2899433a4b6d589fb
Yaniv Cohen <confis@gmail.com>
2022-02-02 16:43:55 +0200

    Update addcrt.sh

--- scripts/addcrt.sh
+++ scripts/addcrt.sh
@@ -3,6 +3,6 @@ File="./domain/domainlist.txt"
 while IFS= read -r line
 do
   cat ./scripts/sample.yml | sed 's/foo.com/'$line'/g' > ./crt/$line.yml
-  echo "domain: ./crt/$line.yml"
+  echo "$line.yml"
   #echo "domain: $line"
 done < "$File"
