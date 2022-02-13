017d941f343be56eb1e6d3fd1be00678d1476fb3
Yaniv Cohen <confis@gmail.com>
2022-02-13 16:22:09 +0200

    Update addcrt.sh

--- scripts/addcrt.sh
+++ scripts/addcrt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-File= chmod +x ./home/runner/work/akamai-cli/akamai-cli/domain/domainlist.txt
+File= "/home/runner/work/akamai-cli/akamai-cli/domain/domainlist.txt"
  while IFS= read -r line
  do
    cat ./scripts/sample.yml | sed 's/foo.com/'$line'/g' > ./crt/$line.yml
