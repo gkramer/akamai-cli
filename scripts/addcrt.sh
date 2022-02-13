da4a8d45ef8c593c394daf890a8c00e328400cdb
Yaniv Cohen <confis@gmail.com>
2022-02-13 11:42:59 +0200

    Update addcrt.sh

--- scripts/addcrt.sh
+++ scripts/addcrt.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-File="./domain/domainlist.txt"
-while IFS= read -r line
-do
-  cat ./scripts/sample.yml | sed 's/foo.com/'$line'/g' > ./crt/$line.yml
-  echo "domain: $line"
-done < "$File"
+
+git add -A;
+git commit -m "$1";
+git push origin $2
+
+
+# File="./domain/domainlist.txt"
+# while IFS= read -r line
+# do
+#   cat ./scripts/sample.yml | sed 's/foo.com/'$line'/g' > ./crt/$line.yml
+#   echo "domain: $line"
+# done < "$File"
